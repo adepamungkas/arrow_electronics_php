7672e754b30a6b670dedcd1c4fa2297cc39360e9
adepamungkas <ade.pamungkas9@gmail.com>
2020-04-03 02:29:27 +0700

    add screen shoot

--- index.php
+++ index.php
@@ -40,9 +40,8 @@
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="index.php">Home</a></li>
-                    <li><a href="download_json.php">Download Json</a></li>
-                    <li><a href="json_to_csv.php">Export CSV</a></li>
-                    <li><a href="raw_json.php">Raw Json</a></li>
+                    <li><a href="download_json.php">Download JSON</a></li>
+                    <li><a href="raw_json.php">Raw JSON</a></li>
 
                 </ul>
 
@@ -59,22 +58,7 @@
         <div class="col-md-12">
             <div class="page-header">
                 <h4>
-                    <form class="form-inline" method="POST" action="index.php">
-                        <div class="form-group">
-                            <div class="input-group">
-
-                                <input type="text" class="form-control" name="cari" placeholder="Cari ..."
-                                       autocomplete="off" value="">
-                                <div class="input-group-addon ">
-                                    <i class="glyphicon glyphicon-search "></i>
-                                </div>
-                            </div>
-                        </div>
-                        <a class="btn btn-success pull-right" href="?page=add">
-                            <i class="glyphicon glyphicon-plus"></i> Tambah
-                        </a>
-                    </form>
-
+                    Call API from Arrow Electronics
                 </h4>
             </div>
 
@@ -82,28 +66,21 @@
 
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Parse Data Jason To HTML</h3>
+                    <h3 class="panel-title">Parse Data JSON To HTML</h3>
                 </div>
                 <div class="panel-body">
                     <?php
 
-                    //$api_url = 'http://dummy.restapiexample.com/api/v1/employees';
+                    //$api_url
                     $api_url = 'http://api.arrow.com//itemservice/v3/en/search/list?req={"request":{"login":"supremecomponents","apikey":"07b23129ead7328ca4f14a9c08fa89f333e30d08042a5ec4d211e7b66851825d","remoteIp":"192.168.102.188","useExact":true,"parts":[{"partNum":"bav99","mfr":"NXP"},{"partNum":"MT47H128M8HQ-3:E"},{"partNum":"TMP275AIDGKT"}]}}';
+
                     // Read JSON file
                     $json_data = file_get_contents($api_url);
 
 
                     // Decode JSON data into PHP array
-                    //$user_data = json_decode($json_data);
                     $jsonDecoded = json_decode($json_data, true);
 
-
-                    //Give our CSV file a name.
-                    $csvFileName = 'example.csv';
-
-                    //Open file pointer.
-                    //$fp = fopen($csvFileName, 'w');
-                    //Loop through the associative array.
                     ?>
 
                     <?php
@@ -111,6 +88,7 @@
 
                         foreach ($serviceresult['data'] as $itemserviceresult) {
                             echo '<hr>';
+                            echo '<b>Result List:</b> </br></br>';
                             foreach ($itemserviceresult['resultList'] as $resultlist) {
 
                                 foreach ($resultlist['resources'] as $resources) {
@@ -119,24 +97,50 @@
 
                                 echo 'Request Part Number : ' . $resultlist['requestedPartNum'] . '<br/>';
                                 echo 'Number Found : ' . $resultlist['numberFound'] . '<br/>';
-                                echo '<hr>';
+
+                                echo '</br>';
+
                                 foreach ($resultlist['PartList'] as $partList) {
+                                    echo '<hr>';
+                                    echo '<b>PartList :</b> </br>';
                                     // var_dump($partList);
                                     echo 'Item Id : ' . $partList['itemId'] . '<br/>';
                                     echo 'partNum : ' . $partList['partNum'] . '<br/>';
+                                    echo '<br/>';
+                                    echo '<hr/>';
+                                    echo '<b>manufacturer :</b> </br>';
                                     foreach ($partList['manufacturer'] as $manufacturer) {
+
                                         echo 'manufacture : ' . $manufacturer . '<br/>';
 
                                     }
                                     echo '<br/>';
+                                    echo '<br/>';
+                                    echo '<b>resources :</b> </br>';
                                     foreach ($partList['resources'] as $resources) {
+
+
                                         echo 'type : ' . $resources['type'] . '<br/>';
                                         echo 'uri : ' . $resources['uri'] . '<br/>';
                                         echo '<br/>';;
                                     }
 
+                                    foreach ($partList['EnvData'] as $EnvData) {
+
+                                        echo '<b>EnvData :</b> </br>';
+                                        foreach ($EnvData as $compliance) {
+                                            echo '<b>compliance :</b> </br>';
+                                            echo 'displayLabel : ' . $compliance['displayLabel'] . '<br/>';
+                                            echo 'displayValue : ' . $compliance['displayValue'] . '<br/>';
+                                            echo '<br/>';;
+                                        }
+                                    }
+
                                     foreach ($partList['InvOrg'] as $InvOrg) {
+                                        echo '<b>InvOrg :</b> </br>';
+
                                         foreach ($InvOrg as $sources) {
+                                            echo '<b>sources :</b> </br>';
 
                                             echo 'currency : ' . $sources['currency'] . '<br/>';
                                             echo 'sourceCd : ' . $sources['sourceCd'] . '<br/>';
@@ -145,14 +149,7 @@
                                         }
                                     }
 
-                                    foreach ($partList['EnvData'] as $EnvData) {
-                                        foreach ($EnvData as $compliance) {
 
-                                            echo 'displayLabel : ' . $compliance['displayLabel'] . '<br/>';
-                                            echo 'displayValue : ' . $compliance['displayValue'] . '<br/>';
-                                            echo '<br/>';;
-                                        }
-                                    }
 
 
                                     echo 'desc : ' . $partList['desc'] . '<br/>';
